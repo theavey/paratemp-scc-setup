90183d91b7c67dfe1a7e3f95fe776fccc42e884c
Thomas Heavey <thomasjheavey@gmail.com>
2019-04-24 18:34:08 -0400

    Setup config file

--- jupyter-scc.py
+++ jupyter-scc.py
@@ -20,6 +20,10 @@ Setup and run Jupyter (for ParaTemp) on SCC from a local machine
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
+import getpass
+import json
+import logging
+import pathlib
 import subprocess
 import sys
 
@@ -29,3 +33,56 @@ except ImportError:
     subprocess.call([sys.executable, "-m", "pip", "install", 'paramiko'])
     import paramiko
 
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+def _setup_log(level=logging.WARNING):
+    global handler
+    handler = logging.StreamHandler()
+    handler.setLevel(level=level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - '
+                                  '%(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
+
+class Config(dict):
+
+    def __init__(self):
+        log.debug('Initializing Config object')
+        self.path = pathlib.Path('~/Library/Application '
+                                 'Support/ParaTemp/settings.json').expanduser()
+        self.temp_path = self.path.with_suffix('.json.new')
+        if self.path.is_file():
+            log.debug('Reading existing config from {}'.format(self.path))
+            d = json.load(self.path.open('r'))
+            super(Config, self).__init__(d)
+        else:
+            log.debug('No existing config found. Creating new')
+            super(Config, self).__init__()
+            self.setup_config()
+            log.info('Wrote new config to {}'.format(self.path))
+
+    def setup_config(self):
+        log.debug('Setting up new configuration')
+        default_username = getpass.getuser()
+        username = input("Username [{}]: ".format(default_username))
+        if len(username) == 0:
+            username = default_username
+        self['username'] = username
+        self['Setup_on_SCC'] = False
+
+    def __setitem__(self, key, value):
+        try:
+            super(Config, self).__setitem__(key, value)
+            json.dump(self, self.temp_path.open('w'), indent=4)
+            self.temp_path.rename(self.path)
+        except Exception:
+            log.exception('Exception raised when trying to write config file!')
+            raise
+
+
+if __name__ == '__main__':
+    _setup_log()
